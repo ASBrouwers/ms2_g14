b5c8da19c2122befd5ef3906503d4080b4abbb8b
MitchellSchijen <m.r.m.j.schijen@student.tue.nl>
2019-03-27 14:39:04 +0100

    altered RDDs in Q2 in alternative method

--- src/main/java/q2.java
+++ src/main/java/q2.java
@@ -59,6 +59,11 @@ public class q2 {
         return inputRDD;
     }
 	
+	public static JavaRDD<String> addIdentifier(JavaRDD input, String id) {
+		JavaRDD<String> output = input.map(row -> row += "," + id);
+		return output;
+	}
+	
 	public static void printFirstLinePairRDD(String desc, JavaPairRDD<Integer, String[]> input) {
 		String output = "\n" + desc + ", first line\n";
 		for (int i = 0; i < input.first()._2.length; i++ ) {
@@ -100,14 +105,16 @@ public class q2 {
         
         JavaRDD<String> textCourseOffers = removeHeader(sc.textFile(startingPath + "CourseOffers.table"));
         JavaRDD<String> textCourseRegistrations = removeHeader(sc.textFile(startingPath + "CourseRegistrations.table"));
+        JavaRDD<String> textCourseOffersId = addIdentifier(textCourseOffers,"CO");
+        JavaRDD<String> textCourseRegistrationsId = addIdentifier(textCourseRegistrations,"CR");
         
-        JavaPairRDD<Integer, String[]> courseOffersPKandTuple = textCourseOffers.mapToPair(
+        JavaPairRDD<Integer, String[]> courseOffersPKandTuple = textCourseOffersId.mapToPair(
                 row -> new Tuple2<>( Integer.valueOf(row.split(",")[0]), row.split(",")) // first column is the PK
         );
         printFirstLinePairRDD("CourseOffers", courseOffersPKandTuple);
         
-        JavaPairRDD<Integer, String[]> courseRegistrationsPKandTuple = textCourseRegistrations.mapToPair(
-                row -> new Tuple2<>( Integer.valueOf(row.split(",")[0]), row.split(",") )// first column is the PK
+        JavaPairRDD<Integer, String[]> courseRegistrationsPKandTuple = textCourseRegistrationsId.mapToPair(
+                row -> new Tuple2<>( Integer.valueOf(row.split(",")[0]), row.split(","))// first column is the PK
         );
         printFirstLinePairRDD("CourseRegistrations",courseRegistrationsPKandTuple);
         
@@ -132,16 +139,122 @@ public class q2 {
         JavaPairRDD<Integer, String[]> fullFilterCourseRegistrations = courseRegistrationsPKandTupleNoNull.filter(row -> Integer.parseInt(row._2()[2]) >= 5);
         printEntirePairRDD("CourseRegistrations, filtered on grade >= 5", fullFilterCourseRegistrations);
         
-        // union courseOffers and courseRegistrations
+        // union courseOffers and courseRegistrations - not done
+        JavaPairRDD<Integer, String[]> CourseOffersAndCourseRegistrationsUnion = fullFilterCourseOffers.union(fullFilterCourseRegistrations);
+        printEntirePairRDD("CourseOffers and CourseRegistrations union result", CourseOffersAndCourseRegistrationsUnion);
         
         // reduce by key on union'd RDD
+        //JavaPairRDD<Integer, String[]> finalRDD = ((Object) CourseOffersAndCourseRegistrationsUnion).reducebyKey((x,integer y) -> x+y);
+        
+
+    	//(Function2<Long, Long, Long>) (a, b) -> a + b
+        
+        // calculate average grade
         
         // shutdown the spark context
         sc.close();
 	}
+	
+	public static Integer[] createIntegerArray(String[] input) {
+		Integer[] result = new Integer[input.length];
+		for (int i = 0; i < result.length; i++) {
+			if (!input[i].contentEquals("null")) {
+				result[i] = Integer.parseInt(input[i]);
+			} else {
+				result[i] = -1;
+			}
+		}
+		return result;
+	}
+	
+	public static void printPartPairRDD(JavaPairRDD<Integer, Integer[]> input, int limit, String description) {
+		int actual_size = (int) input.count();
+		System.out.println(description + "\n RDD has size " + actual_size + "\n");
+		if (actual_size < limit) {
+			limit = actual_size;
+			System.out.println("PairRDD length is smaller than limit parameter");
+			input.foreach(row -> {
+				String output = "key: " + row._1() + ", values: ";
+				for (int i = 0; i < row._2().length; i++) {
+					output += row._2()[i] + ", ";
+				}
+				System.out.println(output);
+			});
+		} else {
+			input.take(limit).forEach(row -> {
+				String output = "key: " + row._1() + ", values: ";
+				for (int i = 0; i < row._2().length; i++) {
+					output += row._2()[i] + ", ";
+				}
+				System.out.println(output);
+			});
+		}
+		
+	}
+	
 
+	public static void query2alternative() {
+		String startingPath; // Folder where table data is located
+		//startingPath = "/tmp/tables/"; 
+		startingPath = "/tmp/tables_reduced/";
+        String master = "local[1]"; // Run locally with 1 thread
+
+        // Setup Spark
+        SparkConf conf = new SparkConf()
+                .setAppName(q2.class.getName())
+                .setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        
+        JavaRDD<String> textCourseOffers = removeHeader(sc.textFile(startingPath + "CourseOffers.table"));
+        JavaRDD<String> textCourseRegistrations = removeHeader(sc.textFile(startingPath + "CourseRegistrations.table"));
+        JavaRDD<String> textCourseOffersId = addIdentifier(textCourseOffers,"0");
+        JavaRDD<String> textCourseRegistrationsId = addIdentifier(textCourseRegistrations,"1");
+        
+        JavaPairRDD<Integer, Integer[]> courseOffers = textCourseOffersId.mapToPair(
+                row -> new Tuple2<>( Integer.valueOf(row.split(",")[0]), createIntegerArray(row.split(","))) // first column is the PK
+        );
+        
+        JavaPairRDD<Integer, Integer[]> courseRegistrations = textCourseRegistrationsId.mapToPair(
+                row -> new Tuple2<>( Integer.valueOf(row.split(",")[0]), createIntegerArray(row.split(",")))// first column is the PK
+        );
+        
+        // filter courseOffers - done    
+        // keep CourseId = var, CourseId is stored in the 2nd column
+        // keep Quartile = var, Quartile is stored in the 4th column
+        // keep Year = var, Year is stored in the 3rd column
+        printPartPairRDD(courseOffers, 40, "CourseOffers, prior to filtering");
+        printPartPairRDD(courseOffers, 2, "CourseOffers, prior to filtering - part");
+        courseOffers = courseOffers.filter(tuple -> tuple._2()[1] == courseId);        
+        courseOffers = courseOffers.filter(tuple -> tuple._2()[3] == quartile);
+        courseOffers = courseOffers.filter(tuple -> tuple._2()[2] == year);
+        printPartPairRDD(courseOffers, 40, "CourseOffers, after filtering");
+        
+        
+        // filter courseRegistrations - done
+        // remove grade = "null" - already done in RDD creation process       
+        // keep grade >= 5
+        printPartPairRDD(courseRegistrations,50,"CourseRegistrations, prior to filtering");
+        courseRegistrations = courseRegistrations.filter(tuple -> tuple._2()[2] >= 5);
+        printPartPairRDD(courseRegistrations,50,"CourseRegistrations, after filtering");        
+        
+        
+        // union courseOffers and courseRegistrations - not done
+        JavaPairRDD<Integer, Integer[]> union = courseOffers.union(courseRegistrations);
+        printPartPairRDD(union, 50, "union");
+        
+        // filter to only keep matching pairs
+        
+        // get sum and count
+        
+        // calculate average grade
+        
+        // shutdown the spark context
+        sc.close();
+	}
+	
 	public static void main(String[] args) {
-		query2();
+		//query2();
+		query2alternative();
 	}
 
 }
