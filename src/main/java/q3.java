6c90c8d0fa7929cb6b8bcae173da17473964734f
A.S.Brouwers <a.s.brouwers@student.tue.nl>
2019-04-03 22:57:00 +0200

    Change structure to reduce number of filters

--- src/main/java/q3.java
+++ src/main/java/q3.java
@@ -3,57 +3,56 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.util.ArrayList;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
 
+import java.util.ArrayList;
 import org.apache.commons.math3.random.MersenneTwister;
-
 import scala.Tuple2;
-import scala.Tuple3;
 
 public class q3 {
-	private static int sqrtNrReducers = 6;
+	private static int sqrtNrReducers = 6; // nr of reducers: square of this value
 	
-	public static JavaRDD removeHeader(JavaRDD inputRDD) {
+	public static JavaRDD<String> removeHeader(JavaRDD<String> inputRDD) {
         String header = (String) inputRDD.first();
         inputRDD = inputRDD.filter(row -> !row.equals(header));
         return inputRDD;
     }
 	
-	public static void q3b() {
-		MersenneTwister rd = new MersenneTwister();
-		
+	public static JavaPairRDD<Integer, Integer> q3b() {
 		String startingPath = "/home/student/Desktop/tables/"; // Folder where table data is located
-        String master = "local[4]"; // Run locally with 1 thread
+        String master = "local[8]"; // Run locally with 1 thread
 
         // Setup Spark
         SparkConf conf = new SparkConf()
                 .setAppName(q1.class.getName())
                 .setMaster(master);
+        
         JavaSparkContext sc = new JavaSparkContext(conf);
         
-        JavaRDD<String> studentRegsText = removeHeader(sc.textFile(startingPath + "CourseRegistrations.table"));
-        JavaRDD<String> quantilePointsText = removeHeader(sc.textFile(startingPath + "QuantilePoints.table.txt"));
+        JavaRDD<String> studentRegsText = sc.textFile(startingPath + "CourseRegistrations2.table");
+        JavaRDD<String> quantilePointsText = sc.textFile(startingPath + "QuantilePoints.table.txt");
         
-        JavaPairRDD<Integer, String> studentRegs = studentRegsText.mapToPair(row -> {
+        // (Grade, G)
+        final String gradeHeader = studentRegsText.first();
+        JavaPairRDD<Integer, String> studentRegs = studentRegsText.filter(row -> !row.contains("null") && !row.equals(gradeHeader)).mapToPair(row -> {
         	String[] split = row.split(",");
-        	return new Tuple2<>(split[2],"G");
-        }).filter(row -> (!row._1.equals("null"))).mapToPair(row -> new Tuple2<>(Integer.valueOf(row._1), row._2));
+        	return new Tuple2<>(Integer.valueOf(split[2]), "G");
+        });
         
-        JavaPairRDD<Integer, String> quantilePoints = quantilePointsText.mapToPair(row -> {
+        // (Quantile, Q)
+        final String quantileHeader = quantilePointsText.first();
+        JavaPairRDD<Integer, String> quantilePoints = quantilePointsText.filter(row -> !row.equals(quantileHeader)).mapToPair(row -> {
         	String[] split = row.split(",");
-        	return new Tuple2<>(split[0], "Q");
-        }).filter(row -> (!row._1.equals("null"))).mapToPair(row -> new Tuple2<>(Integer.valueOf(row._1), row._2));
+        	return new Tuple2<>(Integer.valueOf(split[0]), "Q");
+        });
 		
         // (Grade, G) and (Quantile, Q)
         JavaPairRDD<Integer, String> union = studentRegs.union(quantilePoints);
         
         // Flatmap to get reducer
         JavaPairRDD<Integer, ArrayList<Tuple2<Integer, String>>> withKeys = union.flatMapToPair(row -> {
-        	
         	int[] reducers = q3a(row, sqrtNrReducers);
-        	
         	ArrayList<Tuple2<Integer,ArrayList<Tuple2<Integer, String>>>> l = new ArrayList<>();
         	
         	for (int r : reducers) {
@@ -63,8 +62,6 @@ public class q3 {
         	}
         	return l.iterator();
         });
-        
-        //==============> Sent to reducer
         
         // ArrayList combine all rows at this reducer
         JavaPairRDD<Integer, ArrayList<Tuple2<Integer, String>>> reduced = withKeys.reduceByKey((V1, V2) -> {
@@ -100,8 +97,10 @@ public class q3 {
         	return l.iterator();
         });
         
-        JavaPairRDD<Integer, Integer> result = lessThanQuantile.reduceByKey((v1, v2) -> v1 + v2);
-        result.sortByKey().collect().forEach(row -> System.out.println(row));
+        JavaPairRDD<Integer, Integer> result = lessThanQuantile.reduceByKey((v1, v2) -> v1 + v2).sortByKey();
+        result.collect().forEach(row -> System.out.println(row));
+        sc.close();
+        return result;
 	}
 	
 	private static int[] q3a(Tuple2<Integer, String> row, int sqrtReducers) {
@@ -122,6 +121,8 @@ public class q3 {
 	
 	
 	public static void main(String[] args) {
+		Logger.getLogger("org").setLevel(Level.WARN);
+		Logger.getLogger("akka").setLevel(Level.WARN);
 		q3b();
 	}
 }
